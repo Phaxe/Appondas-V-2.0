238f6506bc678a1becef9db8e18d7424e3cc08db
Phaxe <Yahia.eid@outlook.com>
2023-08-30 12:22:56 +0300

    Projects/Navbar upgrades

--- app/Projects/Portoflio.jsx
+++ app/Projects/Portoflio.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { BsArrowRight } from "react-icons/bs";
-import shop from "../../public/axios.png";
+import shop from "../../public/axios-1.jpg";
 import Image from "next/image";
-import grill from "../../public/grill.png";
+import grill from "../../public/resto.png";
+import Dashboard from "../../public/dashboard.png";
+import Resto from '../../public/resto.png'
+import Rental from '../../public/cars1.png'
+import Gyms from '../../public/gyms.png'
+import Gardens from '../../public/gardens.png'
 
 const Portoflio = () => {
   return (
@@ -48,14 +53,10 @@ const Portoflio = () => {
               UX/UI
             </button>
           </div>
-          <button className='py-2 mt-10 border px-2 text-white border-yahia-700 rounded-md bg-yahia-700'>Check Demo</button>
+          <button className='py-2 mt-10 border px-2 text-white border-white rounded-md bg-yahia-700'>Check Demo</button>
         </div>
 
-        <div
-          className="w-[1000px] h-[600px] flex justify-center items-center mx-auto
-        max-xl:w-[350px] max-xl:h-[300px] max-md:w-[450px] max-sm:w-[340px]
-      "
-        >
+        <div className="w-[1000px] h-full flex justify-center items-center mx-auto">
           <Image
             src={shop}
             className="h-[100%] w-full mx-auto object-left object-cover rounded-md  "
@@ -63,27 +64,55 @@ const Portoflio = () => {
           />
         </div>
       </div>
+    </div>
+  </div>
+  <div className="flex items-center justify-center bg-gradient-to-r from-yahia-400 via-yahia-600 to-yahia-900 rounded-md mb-10 ">
+    <div className="flex items-center  justify-between py-10 gap-10 ">   
+    <div className="w-[1000px] h-full flex justify-center items-center mx-auto">
+          <Image
+            src={Resto}
+            className="h-[100%] w-full mx-auto object-cover rounded-md  "
+            alt="dashboard"
+          />
+        </div>
+      <div className="flex items-center justify-center gap-10  ">
+     
+        <div className=" w-[400px] flex flex-col items-center justify-between pt-4">
+          <div className="flex flex-col gap-10">
+            <h1 className='font-semibold text-white text-center text-4xl'>Resturant Web</h1>
+            <h2 className='text-white text-center font-semibold text-3xl'>Grill</h2>
+            <p className="text-gray-100 pl-4 py-10">
+            A new ecommerce solution for one of the largest wedding dress retailers in the world.
+            </p>
+    
+          </div>
+          <div className="flex gap-4 flex-row">
+            <button className="bg-gray-300 py-2 px-2 rounded-3xl">
+              ReactJs
+            </button>
+            <button className="bg-gray-300 py-2 px-2 rounded-3xl">
+              MongoDb
+            </button>
+            <button className="bg-gray-300 py-2 px-2 rounded-3xl">
+              Redux
+            </button>
+            <button className="bg-gray-300 py-2 px-2 rounded-3xl">
+              UX/UI
+            </button>
+          </div>
+          <button className='py-2 mt-10 border px-2 text-white border-white rounded-md bg-orange-700'>Check Demo</button>
+        </div>
+
+      </div>
  
       
 
     
     </div>
   </div>
-  <div className="flex items-center justify-center bg-gradient-to-r from-yahia-400 via-yahia-600 to-yahia-900 rounded-md mb-10 ">
+  <div className="flex items-center justify-center bg-gradient-to-l from-orange-400 via-orange-600 to-orange-900 rounded-md mb-10 ">
     
-    <div className="flex items-center  justify-between py-10 gap-10 ">
-        
-    <div
-          className="w-[1000px] h-[600px] flex justify-center items-center mx-auto
-        max-xl:w-[350px] max-xl:h-[300px] max-md:w-[450px] max-sm:w-[340px]
-      "
-        >
-          <Image
-            src={grill}
-            className="h-[100%] w-full mx-auto object-left object-cover rounded-md  "
-            alt="dashboard"
-          />
-        </div>
+    <div className="flex items-center  justify-between py-10 ">
       <div className="flex items-center justify-center gap-10  ">
      
         <div
@@ -113,6 +142,163 @@ const Portoflio = () => {
               UX/UI
             </button>
           </div>
+          <button className='py-2 mt-10 border px-2 text-white border-white rounded-md bg-yahia-700'>Check Demo</button>
+        </div>
+
+        <div className="w-[1000px] h-full flex justify-center items-center mx-auto">
+          <Image
+            src={Dashboard}
+            className="h-[100%] w-full mx-auto object-cover rounded-md  "
+            alt="dashboard"
+          />
+        </div>
+      </div>
+ 
+      
+
+    
+    </div>
+  </div>
+  <div className="flex items-center justify-center bg-gradient-to-r from-yahia-400 via-yahia-600 to-yahia-900 rounded-md mb-10 ">
+    
+    <div className="flex items-center  justify-between py-10 gap-10 ">
+        
+    <div className="w-[1000px] h-full flex justify-center items-center mx-auto">
+          <Image
+            src={Rental}
+            className="h-[100%] w-full mx-auto object-cover rounded-md  "
+            alt="dashboard"
+          />
+        </div>
+      <div className="flex items-center justify-center gap-10  ">
+     
+        <div
+          className=" w-[400px] flex flex-col items-center justify-between pt-4
+        
+       "
+        >
+          <div className="flex flex-col gap-10">
+            <h1 className='font-semibold text-white text-center text-4xl'>Resturant Web</h1>
+            <h2 className='text-white text-center font-semibold text-3xl'>Grill</h2>
+            <p className="text-gray-100 pl-4 py-10">
+            A new ecommerce solution for one of the largest wedding dress retailers in the world.
+            </p>
+    
+          </div>
+          <div className="flex gap-4 flex-row">
+            <button className="bg-gray-300 py-2 px-2 rounded-3xl">
+              ReactJs
+            </button>
+            <button className="bg-gray-300 py-2 px-2 rounded-3xl">
+              MongoDb
+            </button>
+            <button className="bg-gray-300 py-2 px-2 rounded-3xl">
+              Redux
+            </button>
+            <button className="bg-gray-300 py-2 px-2 rounded-3xl">
+              UX/UI
+            </button>
+          </div>
+          <button className='py-2 mt-10 border px-2 text-white border-white rounded-md bg-orange-700'>Check Demo</button>
+        </div>
+
+      </div>
+ 
+      
+
+    
+    </div>
+  </div>
+  <div className="flex items-center justify-center bg-gradient-to-l from-orange-400 via-orange-600 to-orange-900 rounded-md mb-10 ">
+    
+    <div className="flex items-center  justify-between py-10 ">
+      <div className="flex items-center justify-center gap-10  ">
+     
+        <div
+          className=" w-[400px] flex flex-col items-center justify-between pt-4
+        
+       "
+        >
+          <div className="flex flex-col gap-10">
+            <h1 className='font-semibold text-white text-4xl'>E-commerce WebApp</h1>
+            <h2 className='text-white text-center font-semibold text-3xl'>Axios Stores</h2>
+            <p className="text-gray-100 pl-4 py-10">
+            A new ecommerce solution for one of the largest wedding dress retailers in the world.
+            </p>
+    
+          </div>
+          <div className="flex gap-4 flex-row">
+            <button className="bg-gray-300 py-2 px-2 rounded-3xl">
+              ReactJs
+            </button>
+            <button className="bg-gray-300 py-2 px-2 rounded-3xl">
+              MongoDb
+            </button>
+            <button className="bg-gray-300 py-2 px-2 rounded-3xl">
+              Redux
+            </button>
+            <button className="bg-gray-300 py-2 px-2 rounded-3xl">
+              UX/UI
+            </button>
+          </div>
+          <button className='py-2 mt-10 border px-2 text-white border-white rounded-md bg-yahia-700'>Check Demo</button>
+        </div>
+
+        <div className="w-[1000px] h-full flex justify-center items-center mx-auto">
+          <Image
+            src={Gyms}
+            className="h-[100%] w-full mx-auto object-cover rounded-md  "
+            alt="dashboard"
+          />
+        </div>
+      </div>
+ 
+      
+
+    
+    </div>
+  </div>
+  <div className="flex items-center justify-center bg-gradient-to-r from-yahia-400 via-yahia-600 to-yahia-900 rounded-md mb-10 ">
+    
+    <div className="flex items-center  justify-between py-10 gap-10 ">
+        
+    <div className="w-[1000px] h-full flex justify-center items-center mx-auto">
+          <Image
+            src={Gardens}
+            className="h-[100%] w-full mx-auto object-cover rounded-md  "
+            alt="dashboard"
+          />
+        </div>
+      <div className="flex items-center justify-center gap-10  ">
+     
+        <div
+          className=" w-[400px] flex flex-col items-center justify-between pt-4
+        
+       "
+        >
+          <div className="flex flex-col gap-10">
+            <h1 className='font-semibold text-white text-center text-4xl'>Resturant Web</h1>
+            <h2 className='text-white text-center font-semibold text-3xl'>Grill</h2>
+            <p className="text-gray-100 pl-4 py-10">
+            A new ecommerce solution for one of the largest wedding dress retailers in the world.
+            </p>
+    
+          </div>
+          <div className="flex gap-4 flex-row">
+            <button className="bg-gray-300 py-2 px-2 rounded-3xl">
+              ReactJs
+            </button>
+            <button className="bg-gray-300 py-2 px-2 rounded-3xl">
+              MongoDb
+            </button>
+            <button className="bg-gray-300 py-2 px-2 rounded-3xl">
+              Redux
+            </button>
+            <button className="bg-gray-300 py-2 px-2 rounded-3xl">
+              UX/UI
+            </button>
+          </div>
+          <button className='py-2 mt-10 border px-2 text-white border-white rounded-md bg-orange-700'>Check Demo</button>
         </div>
 
       </div>
--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -4,10 +4,11 @@ import Link from "next/link";
 import { useState } from "react";
 import { RiMenu3Fill } from "react-icons/ri";
 import { AiOutlineClose } from "react-icons/ai";
+import { usePathname, useRouter } from "next/navigation";
 function NavBar() {
   const [navbar, setNavbar] = useState(false);
   const [isScrolled, setIsScrolled] = useState(false);
-
+  const router = usePathname();
   window.onscroll = () => {
     setIsScrolled(window.pageYOffset === 0 ? false : true);
     return () => (window.onscroll = null);
@@ -20,103 +21,131 @@ function NavBar() {
           isScrolled
             ? "w-full h-[60px] bg-yahia-500 fixed top-0 left-0 duration-300 right-0 z-10"
             : " w-full h-[60px]  duration-300  fixed top-0 left-0 right-0 z-10 bg-yahia-500/[0.7]"
-        }`}>
+        }`}
+      >
         <div
           className={`${
             navbar
               ? "justify-between items-center px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8 max=md:bg-yahia-500/[0.7]"
               : "justify-between items-center px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8 "
-          }`}>
+          }`}
+        >
           <div>
-            <div className='flex items-center justify-between  md:py-3 md:block max-md:py-3'>
+            <div className="flex items-center justify-between  md:py-3 md:block max-md:py-3">
               {/* LOGO */}
-              <Link href='/'>
+              <Link href="/">
                 <Image
-                  src='/logo.png'
+                  src="/logo.png"
                   priority
                   width={100}
                   height={50}
-                  alt='appondas-image'
+                  alt="appondas-image"
                 />
               </Link>
               {/* HAMBURGER BUTTON FOR MOBILE */}
-              <div className='md:hidden'>
+              <div className="md:hidden">
                 <button
-                  className='p-2 text-gray-700hover: rounded-md outline-none focus:border-gray-400 focus:border'
-                  onClick={() => setNavbar(!navbar)}>
+                  className="p-2 text-gray-700hover: rounded-md outline-none focus:border-gray-400 focus:border"
+                  onClick={() => setNavbar(!navbar)}
+                >
                   {navbar ? (
-                    <AiOutlineClose className='flex text-white text-3xl' />
+                    <AiOutlineClose className="flex text-white text-3xl" />
                   ) : (
-                    <RiMenu3Fill className='flex text-white text-3xl' />
+                    <RiMenu3Fill className="flex text-white text-3xl" />
                   )}
                 </button>
               </div>
             </div>
           </div>
-          <div className=''>
+          <div className="">
             <div
               className={`flex-1  justify-center  items-center text-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
                 navbar ? "p-12  md:p-0 block" : "hidden"
-              }`}>
-              <ul className='h-screen items-center justify-center  md:h-auto  md:flex  '>
+              }`}
+            >
+              <ul className="h-screen items-center justify-center  md:h-auto  md:flex  ">
                 <li
-                  className=' text-xl text-white py-1 md:px-4 text-center max-md:border-b-2  rounded-none
+                  className=" text-xl text-white py-1 md:px-4 text-center max-md:border-b-2  rounded-none
                    border-white 
-                 hover:text-white font-semibold  hover:rounded-md'>
-                  <Link className="font-semibold 
-                  duration-300 hover:duration-300 hover:underline
-                  hover:underline-offset-2  hover:bg-clip-text
-                  hover:text-transparent decoration-orange-500
-                  hover:bg-gradient-to-r from-yellow-600 to-red-600 " 
-                     href='/' onClick={() => setNavbar(!navbar)}>
+                 hover:text-white font-semibold  hover:rounded-md"
+                >
+                  <Link
+                  className={` ${
+                    router === "/"
+                      ? "bg-clip-text text-transparent text-transparent decoration-orange-500 bg-gradient-to-r from-yellow-600 to-red-600 underline underline-offset-2 "
+                      : " font-semibold duration-300 hover:duration-300 hover:underline  hover:underline-offset-2  hover:bg-clip-text   hover:text-transparent decoration-orange-500 hover:bg-gradient-to-r from-yellow-600 to-red-600 "
+                  }`}
+                    href="/"
+                    onClick={() => setNavbar(!navbar)}
+                  >
                     Home
                   </Link>
                 </li>
                 <li
-                  className=' text-xl text-white py-1  md:px-4 text-center  
+                  className=" text-xl text-white py-1  md:px-4 text-center  
                    border-white max-md:border-b-2  rounded-none
-                 hover:text-white font-semi-bold '>
-                  <Link className="font-semibold 
-                  duration-300 hover:duration-300 hover:underline
-                  hover:underline-offset-2  hover:bg-clip-text
-                  hover:text-transparent decoration-orange-500
-                  hover:bg-gradient-to-r from-yellow-600 to-red-600 "  href='/Services' onClick={() => setNavbar(!navbar)}>
+                 hover:text-white font-semibold "
+                >
+                  <Link
+                    className={` ${
+                      router === "/Services"
+                        ? "bg-clip-text text-transparent text-transparent decoration-orange-500 bg-gradient-to-r from-yellow-600 to-red-600 underline underline-offset-2 "
+                        : " font-semibold duration-300 hover:duration-300 hover:underline  hover:underline-offset-2  hover:bg-clip-text   hover:text-transparent decoration-orange-500 hover:bg-gradient-to-r from-yellow-600 to-red-600 "
+                    }`}
+                    href="/Services"
+                    onClick={() => setNavbar(!navbar)}
+                  >
                     Services
                   </Link>
                 </li>
                 <li
-                  className=' text-xl text-white py-1 md:px-4 text-center 
+                  className=" text-xl text-white py-1 md:px-4 text-center 
                    border-white max-md:border-b-2   rounded-none
-                 hover:text-white font-semibold'>
-                  <Link className="font-semibold 
-                  duration-300 hover:duration-300 hover:underline
-                  hover:underline-offset-2  hover:bg-clip-text
-                  hover:text-transparent decoration-orange-500
-                  hover:bg-gradient-to-r from-yellow-600 to-red-600 "  href='/Projects' onClick={() => setNavbar(!navbar)}>
+                 hover:text-white font-semibold"
+                >
+                  <Link
+                  className={` ${
+                    router === "/Projects"
+                      ? "bg-clip-text text-transparent text-transparent decoration-orange-500 bg-gradient-to-r from-yellow-600 to-red-600 underline underline-offset-2 "
+                      : " font-semibold duration-300 hover:duration-300 hover:underline  hover:underline-offset-2  hover:bg-clip-text   hover:text-transparent decoration-orange-500 hover:bg-gradient-to-r from-yellow-600 to-red-600 "
+                  }`}
+                    href="/Projects"
+                    onClick={() => setNavbar(!navbar)}
+                  >
                     Projects
                   </Link>
                 </li>
                 <li
-                  className=' text-xl text-white py-1  md:px-4 text-center  
+                  className=" text-xl text-white py-1  md:px-4 text-center  
                    border-white max-md:border-b-2   rounded-none
-                 hover:text-white font-semibold'>
-                  <Link className="font-semibold 
-                  duration-300 hover:duration-300 hover:underline
-                  hover:underline-offset-2  hover:bg-clip-text
-                  hover:text-transparent decoration-orange-500
-                  hover:bg-gradient-to-r from-yellow-600 to-red-600 "  href='/about' onClick={() => setNavbar(!navbar)}>
+                 hover:text-white font-semibold"
+                >
+                  <Link
+                  className={` ${
+                    router === "/about"
+                      ? "bg-clip-text text-transparent text-transparent decoration-orange-500 bg-gradient-to-r from-yellow-600 to-red-600 underline underline-offset-2 "
+                      : " font-semibold duration-300 hover:duration-300 hover:underline  hover:underline-offset-2  hover:bg-clip-text   hover:text-transparent decoration-orange-500 hover:bg-gradient-to-r from-yellow-600 to-red-600 "
+                  }`}
+                    href="/about"
+                    onClick={() => setNavbar(!navbar)}
+                  >
                     About
                   </Link>
                 </li>
                 <li
-                  className=' text-xl text-white py-1  md:px-4 text-center   
+                  className=" text-xl text-white py-1  md:px-4 text-center   
                    border-white max-md:border-b-2   rounded-none
-                 hover:text-white font-semibold'>
-                  <Link className="font-semibold 
-                  duration-300 hover:duration-300 hover:underline
-                  hover:underline-offset-2  hover:bg-clip-text
-                  hover:text-transparent decoration-orange-500
-                  hover:bg-gradient-to-r from-yellow-600 to-red-600 "  href='#contact' onClick={() => setNavbar(!navbar)}>
+                 hover:text-white font-semibold"
+                >
+                  <Link
+                  className={` ${
+                    router === "/contact"
+                      ? "bg-clip-text text-transparent text-transparent decoration-orange-500 bg-gradient-to-r from-yellow-600 to-red-600 underline underline-offset-2 "
+                      : " font-semibold duration-300 hover:duration-300 hover:underline  hover:underline-offset-2  hover:bg-clip-text   hover:text-transparent decoration-orange-500 hover:bg-gradient-to-r from-yellow-600 to-red-600 "
+                  }`}
+                    href="/contact"
+                    onClick={() => setNavbar(!navbar)}
+                  >
                     Contact
                   </Link>
                 </li>
