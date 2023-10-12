c5f6d411b6ab0ca85ee5d039993a54bd5871ef45
moe-founder <moe@appondas.com>
2023-10-12 17:15:56 +0100

    update the banner slogan

--- components/banner/Banner.jsx
+++ components/banner/Banner.jsx
@@ -9,7 +9,7 @@ const Banner = () => {
       </video>
       <div className='absolute left-50 top-2/4 flex-col items-center justify-center text-center'>
         <h1 className='text-7xl text-white font-bold uppercase max-md:text-4xl '>Appondas Quality in Focus</h1>
-        <h4 className='text-white text-2xl pt-3 pb-3'>Helping businesses build thier online homes</h4>
+        <h4 className='text-white text-2xl pt-3 pb-3'>For businesses that values speed, quality and scale</h4>
         <button className='bg-gradient-to-r from-orange-700 via-orange-400 to-orange-700 py-3 px-10 rounded-md text-white'>Learn More</button>
       </div>
      
