bb4fd1b61f9ed07747544293e3bc5f3f0f093c3c
moe-founder <moe@appondas.com>
2023-10-12 17:10:35 +0100

    edit portfolio page content

--- app/pages/portofo/portofo.jsx
+++ app/pages/portofo/portofo.jsx
@@ -64,10 +64,9 @@ const Porto = () => {
         Customers rely on Appondas
         </h1>
         <span className='py-3 xl:px-10 text-white text-center max-lg:text-sm'>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi,
-            pariatur minima? <br /> A quibusdam eius ducimus! Ratione, aut autem omnis
-            porro illo, <br /> ipsam illum placeat temporibus saepe ipsum natus
-            obcaecati voluptates!
+        Appondas is the answer for brands that values speed, quality and scale, our proven track-records<br /> 
+        in complex projects for 50+ fast-growing companies in over 10 different industries.<br /> we are your one-stop shop for all your unique requirements, be
+        it individual resources,<br /> dedicated teams or trunkey project - we got you covered.
         </span>
       </motion.div>
  <motion.div
